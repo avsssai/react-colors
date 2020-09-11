4a8aea6c2f1dde23dfdea116199cac2214690d8b
Shiva Sesha Sai <avsssai@gmail.com>
2020-09-11 18:50:33 +0530

    added color helper logic

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React,{Component} from 'react';
 import Palette from './Palette';
 import seedColors from './colors-seeder';
+import {generatePalette} from './colorHelpers';
 
 class App extends Component {
 
   render() {
+    console.log(generatePalette(seedColors[4]));
     return(
       <div>
         <Palette {...seedColors[4]}/>
