2608a5302b46bfedde91dee30ebf0080ee0079b9
Shiva Sesha Sai <avsssai@gmail.com>
2020-10-04 02:23:50 +0530

    Added delete confirmation dialog

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -4,26 +4,58 @@ import { TransitionGroup, CSSTransition } from "react-transition-group";
 import MiniPalette from "./MiniPalette";
 import { withStyles } from "@material-ui/core/styles";
 
+import Button from "@material-ui/core/Button";
+import Avatar from "@material-ui/core/Avatar";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemAvatar from "@material-ui/core/ListItemAvatar";
+import ListItemText from "@material-ui/core/ListItemText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import Dialog from "@material-ui/core/Dialog";
+import { blue, red } from "@material-ui/core/colors";
+import CheckIcon from "@material-ui/icons/CheckCircle";
+import CloseIcon from "@material-ui/icons/Cancel";
 import styles from "./styles/PaletteListStyles";
 
 class PaletteList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      deleting: false,
+      open: false,
+      toDelete: null,
     };
     this.handlePaletteClick = this.handlePaletteClick.bind(this);
     this.handlePaletteDelete = this.handlePaletteDelete.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+    this.handleDialogOpen = this.handleDialogOpen.bind(this);
   }
   handlePaletteClick(id) {
     // console.log('hello');
     this.props.history.push(`/palette/${id}`);
   }
-  handlePaletteDelete(paletteId) {
-    this.props.handlePaletteDelete(paletteId);
+  handlePaletteDelete() {
+    this.props.handlePaletteDelete(this.state.toDelete);
+    this.setState({
+      open: false,
+      toDelete: null,
+    });
+  }
+  handleClose() {
+    this.setState({
+      open: false,
+      toDelete: null,
+    });
+  }
+  handleDialogOpen(id) {
+    this.setState({
+      open: true,
+      toDelete: id,
+    });
   }
   render() {
     let { palettes, classes } = this.props;
+    let { open } = this.state;
+
     return (
       <div className={classes.root}>
         <div className={classes.container}>
@@ -42,13 +74,51 @@ class PaletteList extends Component {
                     handlePaletteClick={this.handlePaletteClick}
                     id={palette.id}
                     key={palette.id}
-                    handleDelete={this.handlePaletteDelete}
+                    handleDialogOpen={this.handleDialogOpen}
                   />
                 </CSSTransition>
               );
             })}
           </TransitionGroup>
         </div>
+
+        <Dialog
+          onClose={this.handleClose}
+          aria-labelledby="simple-dialog-title"
+          open={open}
+        >
+          <DialogTitle id="simple-dialog-title">Delete palette?</DialogTitle>
+          <List>
+            <ListItem button onClick={this.handlePaletteDelete}>
+              <ListItemAvatar>
+                <Avatar
+                  style={{
+                    background: blue[100],
+                    color: blue[600],
+                    fontSize: "large",
+                  }}
+                >
+                  <CheckIcon />
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText>Yes</ListItemText>
+            </ListItem>
+            <ListItem button onClick={this.handleClose}>
+              <ListItemAvatar>
+                <Avatar
+                  style={{
+                    background: red[100],
+                    color: red[600],
+                    fontSize: "large",
+                  }}
+                >
+                  <CloseIcon />
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText>No</ListItemText>
+            </ListItem>
+          </List>
+        </Dialog>
       </div>
     );
   }
